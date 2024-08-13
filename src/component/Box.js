0d3acf544e48b6220a974feecc4a211adc2dba68
kim bitna <tallshe108@hanmail.net>
2024-08-13 19:48:47 +0900

    modify import bug

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,4 +1,4 @@
-mport React from "react";
+import React from "react";
 
 const Box = (props) => {
   let result;
@@ -6,9 +6,11 @@ const Box = (props) => {
     props.title === "Computer" &&
     props.result !== "tie" &&
     props.result !== ""
-  ) { // 카드가 computer카드인가? && 결과가 비긴건 아닌가? && props.result에 값이 있는가?
+  ) {
+    // 카드가 computer카드인가? && 결과가 비긴건 아닌가? && props.result에 값이 있는가?
     result = props.result === "win" ? "lose" : "win";
-  } else {// 위의 경우가 아니라면 props 로 전달된 결과를 그대로 쓴다.
+  } else {
+    // 위의 경우가 아니라면 props 로 전달된 결과를 그대로 쓴다.
     result = props.result;
   }
   if (props.title === "Computer") {
